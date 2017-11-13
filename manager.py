bda5162f7e3b55849895af94e5722b67674c19ba
wjt <wanjiangtao@digibird.com.cn>
2017-11-13 16:35:57 +0800

    添加main蓝本，并在main蓝本中添加一个restful接口

--- manager.py
+++ manager.py
@@ -1,8 +1,14 @@
 from flask_script import Manager, Shell
 from flask import Flask
+from flask_restful import Api
+from app.main import main as main_blueprint
+from app.main.views import HelloWorld
 
 app = Flask(__name__)
 
+app.register_blueprint(main_blueprint)
+api = Api(app)
+api.add_resource(HelloWorld)
 manager = Manager(app)
 
 
