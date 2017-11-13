c224e9ae92e8f7efeec91bcc76119509ef0b4dc5
wjt <wanjiangtao@digibird.com.cn>
2017-11-13 17:51:05 +0800

    增加/todo_id接口，接口实现get,put方法

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from  flask import Flask
 from flask_restful import Api
 from app.main import main as main_blueprint
-from app.main.views import HelloWorld
+from app.main.views import HelloWorld, TodoSimple
 
 api = Api()
 
 def create_app():
     app = Flask(__name__)
     api.add_resource(HelloWorld, '/')
+    api.add_resource(TodoSimple, '/<string:todo_id>')
     api.init_app(app)
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,16 @@
 from flask_restful import Resource
+from flask import request
 
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+todo = {}
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo: todo[todo_id]}
+
+    def put(self, todo_id):
+        todo[todo_id] = request.form['data']
+        return {todo: todo[todo_id]}
