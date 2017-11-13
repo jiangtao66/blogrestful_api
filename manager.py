f3329c8c35f87d797f9ac98b673831f2ec5dfd81
wjt <wanjiangtao@digibird.com.cn>
2017-11-13 15:43:00 +0800

    添加运行脚本manager.py

---
+++ manager.py
@@ -0,0 +1,15 @@
+from flask_script import Manager, Shell
+from flask import Flask
+
+app = Flask(__name__)
+
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manager.run()
